218ab66c1c100178c457a402a43ecf3123bdab52
will <willzhmic@outlook.com>
2024-11-06 19:58:32 +0800

    Update sum-oushu.java

--- java/sum-oushu.java
+++ java/sum-oushu.java
@@ -1,1 +1,15 @@
-ddd
+package test;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i=1;i<=100;i++){
+            if(i%2 == 0){
+                sum = sum+i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
